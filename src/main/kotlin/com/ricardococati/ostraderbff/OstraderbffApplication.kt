6a8e6cf0f23bc736d7d4e69d08c3ee2eacfc69c6
Ricardo Cocati <ricardo.cocati@ifood.com.br>
2020-06-09 14:38:04 -0300

    primeiro commit

---
+++ src/main/kotlin/com/ricardococati/ostraderbff/OstraderbffApplication.kt
@@ -0,0 +1,11 @@
+package com.ricardococati.ostraderbff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OstraderbffApplication
+
+fun main(args: Array<String>) {
+	runApplication<OstraderbffApplication>(*args)
+}
