c2b3f93aa347060b43e1e78be4e04b0e1ee2bbf8
Ricardo Cocati <ricardo.cocati@ifood.com.br>
2020-06-09 20:49:48 -0300

    Correção da criação do rabbitmq

--- monta_ambiente.sh
+++ monta_ambiente.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 dirMongoDB="/data/mongoDB"
+dirRabbitMQ="/data/rabbitMQ"
 
 if [ -d "$dirMongoDB" ]; then
 	echo "PASTAS JÁ EXISTEM NO SISTEMA" + "$dirMongoDB"
@@ -16,6 +17,22 @@ else
 
 	echo "ALTERANDO O DONO DA PASTA CRIADA" + " $dirMongoDB " + " $USER "
 	sudo chown -c -R $USER "$dirMongoDB"
+fi
+
+if [ -d "$dirRabbitMQ" ]; then
+	echo "PASTAS JÁ EXISTEM NO SISTEMA" + "$dirRabbitMQ"
+else
+	echo "CRIANDO PASTAS DO SISTEMA" + "$dirRabbitMQ"
+	sudo mkdir -p "$dirRabbitMQ"
+
+	echo "AGUARDA 3 SEGUNDOS"
+	sleep 3
+
+	echo "ATRIBUI PERMISSÕES AS PASTAS CRIADAS" + "$dirRabbitMQ"
+	sudo chmod -R 777 "$dirRabbitMQ"
+
+	echo "ALTERANDO O DONO DA PASTA CRIADA" + " $dirRabbitMQ " + " $USER "
+	sudo chown -c -R $USER "$dirRabbitMQ"
 fi
 
 echo "PARANDO MAQUINA DOCKER"
